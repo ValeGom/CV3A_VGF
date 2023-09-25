78fa87fabe1c998fcb758215e00b7a202617e93f
valegom <valeria_9502gf@hotmail.com>
2023-09-25 18:05:13 -0500

    Se agrega el proyecto

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MiApp());
+
+class MiApp extends StatelessWidget {
+  const MiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class Inicio extends StatefulWidget {
+  const Inicio({super.key});
+
+  @override
+  State<Inicio> createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Mi App"),
+        ),
+        body: Center(
+          child: Text("Contenido"),
+        ));
+  }
+}
